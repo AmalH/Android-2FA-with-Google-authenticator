a8172ca69f1481dbd8bcd9e8d271655c3b5ecba0
AmalH <hichri.amal@hotmail.fr>
2018-04-27 21:23:17 +0100

    2FA _First method: using GglAuthenticator in another app _ Final

--- app/src/main/java/amalhichri/androidprojects/com/a2fasample_authyapi/QRCodeActivity.java
+++ app/src/main/java/amalhichri/androidprojects/com/a2fasample_authyapi/QRCodeActivity.java
@@ -3,6 +3,9 @@ package amalhichri.androidprojects.com.a2fasample_authyapi;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -10,6 +13,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -53,5 +57,75 @@ public class QRCodeActivity extends AppCompatActivity {
                     }
                 });
         queue.add(jsObjRequest);
+
+        /** set the imageView's src **/
+
+        ImageView qrCodeImgVw = findViewById(R.id.qrCodeImgVw);
+        Picasso.get().load("https://s3.amazonaws.com/qr-codes-9f266de4dd32a7244bf6862baea01379/h0B0wyANHtW6KKocnwV2H59JNT6eBqUMqVIYYJjYSHc.png").into(qrCodeImgVw);
+
+        /** pass the code entred by user to the Authy API to verify it **/
+        (findViewById(R.id.confirmSignupBtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String codeValidationUrl="https://api.authy.com/protected/json/verify/"+
+                        ((EditText)findViewById(R.id.validationCode)).getText().toString()+"/80749027?api_key=CCb8fPiHfTdFp332cefjTuRjgMNprVOx";
+               /* String codeValidationUrl = "https://api.authy.com/protected/json/verify/"
+                +((EditText)findViewById(R.id.validationCode)).getText().toString()+
+                "80749027"+"?api_key=CCb8fPiHfTdFp332cefjTuRjgMNprVOx";*/
+                String isValid;
+                JSONObject obj = new JSONObject();
+                RequestQueue queue =  AppSingleton.getInstance(getApplicationContext()).getRequestQueue();
+                JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.POST,qrCodeCallUrl,obj,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    if((response.getString("success")).equals("true"))
+                                        Toast.makeText(getApplicationContext(), "Registered: ", Toast.LENGTH_LONG).show();
+                                    else
+                                        Toast.makeText(getApplicationContext(), "Not registered!", Toast.LENGTH_LONG).show();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.e("ERROR! ",error.getMessage());
+                            }
+                        });
+                queue.add(jsObjRequest);
+            }
+        });
     }
+
+    /*private void firebaseSignup(final String email, String password){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        // add user to database
+                        Log.d("Test","Facebook to firebase success");
+                        amalhichri.androidprojects.com.a2fasample_authyapi.Utils.User userToAdd = new amalhichri.androidprojects.com.a2fasample_authyapi.Utils.User(username,email);
+                        FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(userToAdd).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                //Log.d("Failure",e.getMessage());
+                            }
+                        });
+                        Toast.makeText(getApplicationContext(), "Successfully registered!", Toast.LENGTH_LONG).show();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                startActivity(new Intent(getApplicationContext(), SigninActivity.class));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("ERROR:",e.getMessage());
+            }
+        });
+    }*/
 }
--- app/src/main/java/amalhichri/androidprojects/com/a2fasample_authyapi/TwoFactorAuthActivity.java
+++ app/src/main/java/amalhichri/androidprojects/com/a2fasample_authyapi/TwoFactorAuthActivity.java
@@ -2,12 +2,10 @@ package amalhichri.androidprojects.com.a2fasample_authyapi;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -15,13 +13,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -79,7 +70,7 @@ public class TwoFactorAuthActivity extends AppCompatActivity {
                                     // get the returned id
                                     JsonObject addedUser = gson.fromJson(response.getString("user"),JsonObject.class);
                                     addedUserId = (addedUser.get("id")).getAsString();
-                                    Toast.makeText(getApplicationContext(), "Res: "+addedUserId, Toast.LENGTH_LONG).show();
+                                   // Toast.makeText(getApplicationContext(), "Res: "+addedUserId, Toast.LENGTH_LONG).show();
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
@@ -95,8 +86,7 @@ public class TwoFactorAuthActivity extends AppCompatActivity {
 
                 /** 3.Call the Authy API to generate a QRCode **/
                 // and pass it to next activity because it will be user
-                qrCodeCallUrl="https://api.authy.com/protected/json/users/"+addedUserId+
-                        "/secret?api_key=CCb8fPiHfTdFp332cefjTuRjgMNprVOx";
+                qrCodeCallUrl="https://api.authy.com/protected/json/users/80749027/secret?api_key=CCb8fPiHfTdFp332cefjTuRjgMNprVOx";
                 Intent qrCodeIntent = new Intent(getApplicationContext(), QRCodeActivity.class);
                 qrCodeIntent.putExtra("qrCodeCallUrl",qrCodeCallUrl);
                 startActivity(qrCodeIntent);
@@ -158,32 +148,5 @@ public class TwoFactorAuthActivity extends AppCompatActivity {
     }
 
 
-    private void firebaseSignup(final String email, String password){
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        // add user to database
-                        Log.d("Test","Facebook to firebase success");
-                        amalhichri.androidprojects.com.a2fasample_authyapi.Utils.User userToAdd = new amalhichri.androidprojects.com.a2fasample_authyapi.Utils.User(username,email);
-                        FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(userToAdd).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                //Log.d("Failure",e.getMessage());
-                            }
-                        });
-                        Toast.makeText(getApplicationContext(), "Successfully registered!", Toast.LENGTH_LONG).show();
-                    }
-                }).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                startActivity(new Intent(getApplicationContext(), SigninActivity.class));
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d("ERROR:",e.getMessage());
-            }
-        });
-    }
+
 }
